20d623e8af10971dec16d8916b87f98454f9a8ab
Kolb <contact@kolbxyz.xyz>
2026-08-29 21:07:51 +0200

    chore(release): bump version to 1.1.1 (versionCode 12) for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.wander.android"
         minSdk = 26
         targetSdk = 37
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 12
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
