bfae539bee736372a23f84c1c53ce7e1518f3288
Kolbxyz <105014128+Kolbxyz@users.noreply.github.com>
2026-09-14 09:56:49 +0200

    chore: bump to 1.5.0-beta.22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.wander.android"
         minSdk = 26
         targetSdk = 37
-        versionCode = 37
-        versionName = "1.5.0-beta.21"
+        versionCode = 38
+        versionName = "1.5.0-beta.22"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
